797170975918f134fac0c2a5252f69bfbb4483a7
Hans Sérgio <hanssergiom@gmail.com>
2022-03-08 21:51:07 -0300

    ctd

---
+++ Conversão/src/conv/dinheiro/conversor.java
@@ -0,0 +1,14 @@
+package conv.dinheiro;
+
+public class conversor {
+
+public float convdolar (float dolar, float vreais) {
+		
+		float resultado;
+		resultado = dolar*vreais;
+		return resultado;
+		
+	}
+	
+	
+}
---
+++ Conversão/src/conv/dinheiro/main.java
@@ -0,0 +1,29 @@
+package conv.dinheiro;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		float mostrar = 0, dolar, vreais;
+		conversor converte = new conversor();
+		
+		System.out.println("DIGITE O VALOR DO DOLAR");
+		dolar = sc.nextFloat();
+		 
+		System.out.println("DIGITE O VALOR DE REAIS A SEREM CONVERTIDOS");
+		vreais = sc.nextFloat();
+		
+		mostrar = converte.convdolar(dolar, vreais);
+		
+		System.out.println("o valor convertido é " + mostrar);		 
+		
+		
+		
+		sc.close();
+	}
+
+}
