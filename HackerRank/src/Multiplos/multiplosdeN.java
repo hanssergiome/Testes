de61cb9404e14188fbd78539c389fa5b4f8d2b92
Hans Sérgio <hanssergiom@gmail.com>
2022-03-10 20:31:21 -0300

    exercicio HackerRank

---
+++ HackerRank/src/Multiplos/multiplosdeN.java
@@ -0,0 +1,16 @@
+package Multiplos;
+
+import java.util.Scanner;
+
+public class multiplosdeN {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(N + " x " + i + " = " + i * N);
+		}
+
+	}
+}
