2d096e2baaa2f1dc85959a6d4391035f314ca40c
Hans Sérgio <hanssergiom@gmail.com>
2022-03-11 23:45:22 -0300

    CTD de hoje

---
+++ HackerRank2/src/LoopsJava.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class LoopsJava {
+
+	public static void main(String[] args) {
+		
+		    	
+		        Scanner sc = new Scanner(System.in);
+		        int a = sc.nextInt();
+
+		        for(int i = 1; i <= 10; i++){
+
+		            System.out.printf("%d x %d = %d%n", a, i, a*i);
+		        }
+		    
+		
+		    }
+	}
+	
+
