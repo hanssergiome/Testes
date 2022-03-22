233d8dbc68ddf439d1c84025e04e758ec81a2ef3
Hans Sérgio <hanssergiom@gmail.com>
2022-03-21 21:42:30 -0300

    CTD

---
+++ HRStaticIinizalizer/src/teste/StaticInializer.java
@@ -0,0 +1,29 @@
+package teste;
+
+import java.util.Scanner;
+
+public class StaticInializer {
+
+	static int B, H;
+	static boolean flag = true;
+	static {
+		Scanner sc = new Scanner(System.in);
+		B = sc.nextInt();
+		H = sc.nextInt();
+		if (B <= 0 || H <= 0) {
+			flag = false;
+			System.out.println("java.lang.Exception: Breadth and height must be positive");
+			sc.close();
+		}
+	}
+
+	public static void main(String[] args) {
+
+		if (flag) {
+			int area = B * H;
+			System.out.print(area);
+
+		}
+	}
+
+}
