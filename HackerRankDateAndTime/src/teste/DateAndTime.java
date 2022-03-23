73099a9d827db3a7735182b0fe0ada5b5c73681d
Hans Sérgio <hanssergiom@gmail.com>
2022-03-22 21:06:12 -0300

    ctd

---
+++ HackerRankDateAndTime/src/teste/DateAndTime.java
@@ -0,0 +1,27 @@
+package teste;
+
+import java.util.Calendar;
+
+public class DateAndTime {
+	
+
+    public static String findDay(int month, int day, int year) {
+
+
+        Calendar cal = Calendar.getInstance();
+
+        cal.set(Calendar.MONTH, month-1);
+
+        cal.set(Calendar.DAY_OF_MONTH, day);
+
+        cal.set(Calendar.YEAR, year);
+
+        String[] day_of_week = {"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY","SATURDAY"};
+       
+
+        return day_of_week[cal.get(Calendar.DAY_OF_WEEK)-1];
+
+
+    }
+	
+}
