b0cf6b0f4a98739d5cec8db465bb5b3897b6bd9d
Hans Sérgio <hanssergiom@gmail.com>
2022-03-29 23:55:53 -0300

    ctd

--- HackerRankJava1dArray/src/teste/OneDarray.java
+++ HackerRankJava1dArray/src/teste/OneDarray.java
@@ -1,31 +1,37 @@
 package teste;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class OneDarray {
 
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 
-		Scanner scan = new Scanner(System.in);
-
-		int n = scan.nextInt();
-
-		int[] a = new int[n];
-
+		List<List<Integer>> lines = new ArrayList<List<Integer>>();
+		int n = in.nextInt();
 		for (int i = 0; i < n; i++) {
-
-			a[i] = scan.nextInt();
-
+			List<Integer> line = new ArrayList<Integer>();
+			int d = in.nextInt();
+			for (int j = 0; j < d; j++) {
+				line.add(in.nextInt());
+			}
+			lines.add(line);
 		}
 
-		scan.close();
-
-		for (int i = 0; i < a.length; i++) {
-
-			System.out.println(a[i]);
-
+		int q = in.nextInt();
+		for (int i = 0; i < q; i++) {
+			int x = in.nextInt();
+			int y = in.nextInt();
+			if (y > lines.get(x - 1).size()) {
+				System.out.println("ERROR!");
+			} else {
+				System.out.println(lines.get(x - 1).get(y - 1));
+			}
 		}
 
+		in.close();
 	}
 
 }
