d9d13174e406a59b47ae0761d7a26c508551eaa7
Hans Sérgio <hanssergiom@gmail.com>
2022-04-05 23:05:57 -0400

    ctd

---
+++ HackerRankSyntaxCheck/src/teste/SyntaxChecker.java
@@ -0,0 +1,28 @@
+package teste;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class SyntaxChecker {
+	
+	
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while(testCases>0){
+			String pattern = in.nextLine();
+          	//Write your code
+              try {
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            } catch (PatternSyntaxException e) {
+                System.out.println("Invalid");
+            }
+            testCases--;
+            in.close();
+		}
+	}
+}
+
+
