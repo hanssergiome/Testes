390f4403eff170984261afec6c4709861cc464a6
Hans Sérgio <hanssergiom@gmail.com>
2022-04-13 19:17:23 -0300

    correção código, + adesão desafio

---
+++ HackerRankJavaRegex/src/teste/JavaRegex.java
@@ -0,0 +1,27 @@
+package teste;
+
+import java.util.Scanner;
+
+public class JavaRegex {
+
+	
+
+	    public static void main(String[] args){
+	        Scanner sc = new Scanner(System.in);
+	        while(sc.hasNext()){
+	            String IP = sc.next();
+	            System.out.println(IP.matches(new MyRegex().pattern));
+	            sc.close();
+	        }
+
+	    }
+	}
+
+	class MyRegex {
+
+	String ip0to255 = "(\\d{1,2}||(0|1)\\d{2}||2[0-4]\\d||25[0-5])";
+
+	public String pattern = ip0to255 + "\\." + ip0to255 + "\\." + ip0to255 + "\\." + ip0to255; 
+	
+	}
+
