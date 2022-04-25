a0d1d626eb4b65cf4bb5efdd4c2f7b88aaa3dd15
Hans Sérgio <hanssergiom@gmail.com>
2022-04-24 23:35:43 -0400

    ctd

--- HackerRankJavaAbstractClass/src/teste/JavaAbstract.java
+++ HackerRankJavaAbstractClass/src/teste/JavaAbstract.java
@@ -28,6 +28,7 @@ import java.util.Scanner;
 			MyBook new_novel=new MyBook();
 			new_novel.setTitle(title);
 			System.out.println("The title is: "+new_novel.getTitle());
+			System.out.println("");
 	      	sc.close();
 	      	
 			
