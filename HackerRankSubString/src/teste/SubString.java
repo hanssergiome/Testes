d7339f54bd580cc647c4b16bbc1fab7f8f3c179e
Hans Sérgio <hanssergiom@gmail.com>
2022-04-25 23:13:50 -0400

    problem fix

--- HackerRankSubString/src/teste/SubString.java
+++ HackerRankSubString/src/teste/SubString.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class SubString {
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		String S = in.next();
-		int start = in.nextInt();
-		int end = in.nextInt();
+		Scanner sc = new Scanner(System.in);
+		String S = sc.next();
+		int start = sc.nextInt();
+		int end = sc.nextInt();
 
 		System.out.println(S.substring(start, end));
-		in.close();
+		sc.close();
 	}
 
 }
