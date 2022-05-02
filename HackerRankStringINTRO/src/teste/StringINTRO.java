3d7feecbe808f3fad2efd32ad5447549b673ead2
Hans Sérgio <hanssergiom@gmail.com>
2022-05-01 23:50:44 -1000

    ctd

--- HackerRankStringINTRO/src/teste/StringINTRO.java
+++ HackerRankStringINTRO/src/teste/StringINTRO.java
@@ -5,19 +5,17 @@ import java.util.Scanner;
 public class StringINTRO {
 
 	public static void main(String[] args) {
-		
-		        Scanner sc=new Scanner(System.in);
-		        String a = sc.next();
-		        String b = sc.next();
-		        sc.close();
-		        
-		        System.out.println( a.length() + b.length() );
-		        System.out.println( (b.compareTo(b) > 0) ? "Yes" : "No");
-		        System.out.println(
-		            a.substring(0, 1).toUpperCase() + a.substring(1) + " " +
-		            b.substring(0, 1).toUpperCase() + b.substring(1)
-		        );
-		    }
-		}
-		                        
-	
+
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		String b = sc.next();
+		sc.close();
+
+		System.out.println(a.length() + b.length());
+		System.out.println((b.compareTo(b) > 0) ? "Yes" : "No");
+		System.out.println(
+
+				a.substring(0, 1).toUpperCase() + a.substring(1) + " " + b.substring(0, 1).toUpperCase()
+						+ b.substring(1));
+	}
+}
