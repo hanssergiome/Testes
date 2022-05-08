08e28c77ed08ab5c54f52e77e4f8f1850155a1d2
Hans Sérgio <hanssergiom@gmail.com>
2022-05-07 22:02:44 -1000

    ctd

--- HackerRankJavaAbstractClass/src/teste/JavaAbstract.java
+++ HackerRankJavaAbstractClass/src/teste/JavaAbstract.java
@@ -2,36 +2,33 @@ package teste;
 
 import java.util.Scanner;
 
+abstract class Book {
+	String title;
 
-	abstract class Book{
-		String title;
-		abstract void setTitle(String s);
-		String getTitle(){
-			return title;
-		}
+	abstract void setTitle(String s);
+
+	String getTitle() {
+		return title;
 	}
+}
 
-
-	class MyBook extends Book {
-	    @Override
-	    void setTitle(String s) {
-	        title = s;
-	    }
+class MyBook extends Book {
+	@Override
+	void setTitle(String s) {
+		title = s;
 	}
+}
 
+public class JavaAbstract {
+	public static void main(String[] args) {
 
-	public class JavaAbstract {
-		public static void main(String []args){
-		
-			Scanner sc=new Scanner(System.in);
-			String title=sc.nextLine();
-			MyBook new_novel=new MyBook();
-			new_novel.setTitle(title);
-			System.out.println("The title is: "+new_novel.getTitle());
-			System.out.println("");
-	      	sc.close();
-	      	
-			
-		}
+		Scanner sc = new Scanner(System.in);
+		String title = sc.nextLine();
+		MyBook new_novel = new MyBook();
+		new_novel.setTitle(title);
+		System.out.println("The title is: " + new_novel.getTitle());
+		System.out.println("");
+		sc.close();
+
 	}
-
+}
