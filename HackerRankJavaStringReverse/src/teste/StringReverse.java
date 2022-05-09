3e36508d3e9680cbd67bdcc53bd380fe1a4845f9
Hans Sérgio <hanssergiom@gmail.com>
2022-05-08 23:33:33 -0300

    ctd

--- HackerRankJavaStringReverse/src/teste/StringReverse.java
+++ HackerRankJavaStringReverse/src/teste/StringReverse.java
@@ -10,10 +10,15 @@ public class StringReverse {
 		String A = sc.next();
 
 		for (int i = 1; i < A.length() / 2; i++) {
+
 			if (A.charAt(i) == A.charAt(A.length() - i - 1)) {
+
 				System.out.println("Yes");
+
 			} else {
+
 				System.out.println("No");
+
 			}
 		}
 		sc.close();
