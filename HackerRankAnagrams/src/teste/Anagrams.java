41f142b7abb3fac9c63d078c6f97b5935cf2d21f
Hans Sérgio <hanssergiom@gmail.com>
2022-05-09 23:56:41 -0300

    ctd

--- HackerRankAnagrams/src/teste/Anagrams.java
+++ HackerRankAnagrams/src/teste/Anagrams.java
@@ -6,21 +6,21 @@ import java.util.Scanner;
 public class Anagrams {
 
 	static boolean isAnagram(String A, String B) {
-			   A=A.toLowerCase();
-			   B=B.toLowerCase();
-			   boolean f = false;
-			  char[] c = A.toCharArray();
-			  Arrays.sort(c);
-			  char[] d = B.toCharArray();
-			  Arrays.sort(d);
-			  String a = new String (c);
-		      String b = new String (d);
-		      if (a.equals(b)) {
-		    	  f=true;
-		      }
-		      return f;
-		   
+		A = A.toLowerCase();
+		B = B.toLowerCase();
+		boolean f = false;
+		char[] c = A.toCharArray();
+		Arrays.sort(c);
+		char[] d = B.toCharArray();
+		Arrays.sort(d);
+		String a = new String(c);
+		String b = new String(d);
+		if (a.equals(b)) {
+			f = true;
 		}
+		return f;
+
+	}
 
 	public static void main(String[] args) {
 
@@ -30,8 +30,8 @@ public class Anagrams {
 		sc.close();
 		boolean ret = isAnagram(a, b);
 		System.out.println((ret) ? "Anagrams" : "Not Anagrams");
-		
-	}
+		sc.close();
 
 	}
 
+}
