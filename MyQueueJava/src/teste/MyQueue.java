30893c2cbb547bc57b84e8ee7c45e3707951fea0
Hans Sérgio <hanssergiom@gmail.com>
2022-05-14 02:39:32 -0300

    ctd

---
+++ MyQueueJava/src/teste/MyQueue.java
@@ -0,0 +1,24 @@
+package teste;
+
+import java.util.PriorityQueue;
+
+public class MyQueue {
+
+	
+
+	public class Main {
+		public static void main(String[] args)
+		{
+			PriorityQueue<Integer> queue = new PriorityQueue<>();
+			queue.add(4);
+			queue.add(3);
+			queue.add(2);
+			queue.add(1);
+
+			while (queue.isEmpty() == false) {
+				System.out.printf("%d", queue.remove());
+			}
+		}
+	}
+	
+}
