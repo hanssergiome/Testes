06420584854a866fd11eae0726b9a8035385b43f
Hans Sérgio <hanssergiom@gmail.com>
2022-05-18 18:08:55 -0300

    ctd

--- HackerRankJavaDateAndTime/src/teste/JavaDateAndTime.java
+++ HackerRankJavaDateAndTime/src/teste/JavaDateAndTime.java
@@ -8,14 +8,16 @@ import java.io.InputStreamReader;
 
 import javax.naming.spi.DirStateFactory.Result;
 
-public static String findDay(int month,int day,int year){
-	
-Calendar cal=Calendar.getInstance();cal.set(Calendar.MONTH,month-1);{
-cal.set(Calendar.DAY_OF_MONTH,day);cal.set(Calendar.YEAR,year);
-String dayOfWeek=cal.getDisplayName(Calendar.DAY_OF_WEEK,Calendar.LONG,Locale.US).toUpperCase();return dayOfWeek;
-
+public static String findDay(int month, int day, int year) {
+    Calendar cal = Calendar.getInstance();
+    cal.set(Calendar.MONTH,month-1);
+    cal.set(Calendar.DAY_OF_MONTH,day);
+    cal.set(Calendar.YEAR,year);
+String dayOfWeek = cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.US).toUpperCase();
+    return dayOfWeek;
 }
 }
+s
 
 public class JavaDateAndTime {
 	public static void main(String[] args) throws IOException {
