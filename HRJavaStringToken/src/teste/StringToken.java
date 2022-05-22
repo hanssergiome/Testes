3bb245559b6f2537a9330c03a96dd4b1e17e7540
Hans Sérgio <hanssergiom@gmail.com>
2022-05-22 19:32:13 -0300

    ctd

--- HRJavaStringToken/src/teste/StringToken.java
+++ HRJavaStringToken/src/teste/StringToken.java
@@ -7,6 +7,14 @@ public class StringToken {
 	         String S = sc.nextLine();
 	         String[] tokens = S.split("[^a-zA-Z]");
 	         int numTokens = 0;
+	         
+	         
+	         for (int i=0; i<tokens.length; ++i) 
+	             if (tokens[i].length() > 0) 
+	                 numTokens++;
+	             
+	         System.out.println(numTokens);
+	        
 
 }
 }
