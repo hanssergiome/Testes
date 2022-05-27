0878a838cb8cc1d0e4506e48fd6fcd4b3068151e
Hans Sérgio <hanssergiom@gmail.com>
2022-05-27 19:45:36 -0300

    ctd

--- HRJavaReverseString/src/teste/ReverseString.java
+++ HRJavaReverseString/src/teste/ReverseString.java
@@ -3,6 +3,17 @@ package teste;
 public class ReverseString {
 
 	
+	public class Solution {
+	    public static void main(String[] args) {
+	        /* Read input */
+	        Scanner scan = new Scanner(System.in);
+	        String str = scan.nextLine();
+	        scan.close();
+	        
+	        /* Reverse string and compare to original */
+	        String reversed = new StringBuilder(str).reverse().toString();
+	        System.out.println(str.equals(reversed) ? "Yes" : "No");
+	    }
 	
-	
+}
 }
