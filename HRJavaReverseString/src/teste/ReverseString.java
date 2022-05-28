7562d53f3ceccf814a6c69588a8af340f1f6151f
Hans Sérgio <hanssergiom@gmail.com>
2022-05-28 19:49:09 -0300

    ctd

--- HRJavaReverseString/src/teste/ReverseString.java
+++ HRJavaReverseString/src/teste/ReverseString.java
@@ -1,18 +1,21 @@
 package teste;
 
+import java.util.Scanner;
+
 public class ReverseString {
 
 	
 	public class Solution {
 	    public static void main(String[] args) {
 	        /* Read input */
-	        Scanner scan = new Scanner(System.in);
-	        String str = scan.nextLine();
-	        scan.close();
+	        Scanner sc = new Scanner(System.in);
+	        String str = sc.nextLine();
+	        
 	        
 	        /* Reverse string and compare to original */
 	        String reversed = new StringBuilder(str).reverse().toString();
 	        System.out.println(str.equals(reversed) ? "Yes" : "No");
+	        sc.close();
 	    }
 	
 }
