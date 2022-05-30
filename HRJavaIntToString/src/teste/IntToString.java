bd233d500a2bc1ea144d37bb9fb4c60ae222de8e
Hans Sérgio <hanssergiom@gmail.com>
2022-05-30 19:51:07 -0300

    ctd

--- HRJavaIntToString/src/teste/IntToString.java
+++ HRJavaIntToString/src/teste/IntToString.java
@@ -1,5 +1,15 @@
 package teste;
 
-public class IntToString {
+import java.util.Scanner;
 
+public class IntToString {
+	
+        
+	   try {
+	   Scanner sc = new Scanner(System.in);
+	   int n = sc .nextInt();
+	   sc.close();
+	  
+	String s = "" + n;
+}
 }
