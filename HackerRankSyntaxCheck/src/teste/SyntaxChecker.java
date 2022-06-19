8e240a552fc1636003a9387d63dcb8e71d1fdd14
Hans Sérgio <hanssergiom@gmail.com>
2022-06-19 10:26:10 -0300

    ctd

--- HackerRankSyntaxCheck/src/teste/SyntaxChecker.java
+++ HackerRankSyntaxCheck/src/teste/SyntaxChecker.java
@@ -5,24 +5,21 @@ import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
 public class SyntaxChecker {
-	
-	
-	public static void main(String[] args){
+
+		public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int testCases = Integer.parseInt(sc.nextLine());
-		while(testCases>0){
+		while (testCases > 0) {
 			String pattern = sc.nextLine();
-
-              try {
-                Pattern.compile(pattern);
-                System.out.println("Valid");
-            } catch (PatternSyntaxException e) {
-                System.out.println("Invalid");
-            }
-            testCases--;
-            sc.close();
+			
+			try {
+				Pattern.compile(pattern);
+				System.out.println("Valid");
+			} catch (PatternSyntaxException e) {
+				System.out.println("Invalid");
+				sc.close();
+			}
+			testCases--;
 		}
 	}
 }
-
-
