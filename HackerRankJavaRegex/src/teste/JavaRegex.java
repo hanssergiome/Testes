c2382bb4578707a2cefa1c67a12bb5af84eb4e13
Hans Sérgio <hanssergiom@gmail.com>
2022-06-20 10:28:36 -0300

    ctd

--- HackerRankJavaRegex/src/teste/JavaRegex.java
+++ HackerRankJavaRegex/src/teste/JavaRegex.java
@@ -4,24 +4,20 @@ import java.util.Scanner;
 
 public class JavaRegex {
 
-	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		while (in.hasNext()) {
+			String IP = in.next();
+			System.out.println(IP.matches(new MyRegex().pattern));
+		}
 
-	    public static void main(String[] args){
-	        Scanner sc = new Scanner(System.in);
-	        while(sc.hasNext()){
-	            String IP = sc.next();
-	            System.out.println(IP.matches(new MyRegex().pattern));
-	            sc.close();
-	        }
-
-	    }
 	}
 
 	class MyRegex {
 
-	String ip0to255 = "(\\d{1,2}||(0|1)\\d{2}||2[0-4]\\d||25[0-5])";
+		String ip0to255 = "(\\d{1,2}||(0|1)\\d{2}||2[0-4]\\d||25[0-5])";
 
-	public String pattern = ip0to255 + "\\." + ip0to255 + "\\." + ip0to255 + "\\." + ip0to255; 
-	
+		public String pattern = ip0to255 + "\\." + ip0to255 + "\\." + ip0to255 + "\\." + ip0to255;
+
 	}
-
+}
